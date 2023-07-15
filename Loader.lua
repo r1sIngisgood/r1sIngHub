3a870280daf7d19b43c6fa6769c3e0cf5ce04d19
r1sIngisgood <91692710+r1sIngisgood@users.noreply.github.com>
2023-07-16 00:57:28 +0300

    renamed hub to Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,14 +1,9 @@
 local UiLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlexR32/Bracket/main/BracketV32.lua"))()
 
 print("1")
-local Window = UiLib:Window({Name = "Window",Enabled = true,Color = Color3.new(1,0.5,0.25),Size = UDim2.new(0,496,0,496),Position = UDim2.new(0.5,-248,0.5,-248)})
-Window:ChangeName("r1sIngHub")
-Window:ChangeSize(UDim2.new(0,500,0,500))
-Window:ChangePosition(UDim2.new(0.5,0,0.5,0))
+local Window = UiLib:Window({Name = "r1sIngHub",Enabled = true,Color = Color3.new(1,0.5,0.25),Size = UDim2.new(0,496,0,496),Position = UDim2.new(0.5,-248,0.5,-248)})
 
-local ScriptsTab = Window:Tab()
- ScriptsTab:ChangeName("Scripts")
-ScriptsTab:Divider({Text = "Scripts", Side = "Left"})
+local ScriptsTab = Window:Tab({Name = "Scripts"})
 
 local DexButton = ScriptsTab:Button({Name = "Dex Explorer", Side = "Left", Callback = function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/r1sIngisgood/r1sIngHub/main/Scripts/DexExplorer.lua", true))()
