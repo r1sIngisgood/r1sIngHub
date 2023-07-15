cdd0cd7cc6fa13be3f266f6d352e07ec29d4fbe2
r1sIngisgood <91692710+r1sIngisgood@users.noreply.github.com>
2023-07-15 20:55:56 +0300

    Update r1sIngHub.lua

--- r1sIngHub.lua
+++ r1sIngHub.lua
@@ -1,18 +1,18 @@
 local UiLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/r1sIngisgood/r1sIngHub/main/UiLibs/Bracket.lua"))
 
-local Window = UiLib:Window() do
-    Window:ChangeName("r1sIngHub")
-    Window:ChangeSize(UDim2.new(0,500,0,500))
-    Window:ChangePosition(UDim2.new(0.5,0,0.5,0))
-    local ScriptsTab = Window:Tab() do
-        ScriptsTab:ChangeName("Scripts")
-        ScriptsTab:Divider({Text = "Scripts", Side = "Left"})
+local Window = UiLib:Window()
+Window:ChangeName("r1sIngHub")
+Window:ChangeSize(UDim2.new(0,500,0,500))
+Window:ChangePosition(UDim2.new(0.5,0,0.5,0))
 
-        local DexButton = ScriptsTab:Button({Name = "Dex Explorer", Side = "Left", Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/r1sIngisgood/r1sIngHub/main/Scripts/DexExplorer.lua", true))()
-        end})
-        local RemoteSpy = ScriptsTab:Button({Name = "RemoteSpy", Side = "Left", Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/r1sIngisgood/r1sIngHub/main/Scripts/RemoteSpy.lua", true))()
-        end})
-    end
-end
+local ScriptsTab = Window:Tab()
+ ScriptsTab:ChangeName("Scripts")
+ScriptsTab:Divider({Text = "Scripts", Side = "Left"})
+
+local DexButton = ScriptsTab:Button({Name = "Dex Explorer", Side = "Left", Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/r1sIngisgood/r1sIngHub/main/Scripts/DexExplorer.lua", true))()
+end})
+
+local RemoteSpy = ScriptsTab:Button({Name = "RemoteSpy", Side = "Left", Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/r1sIngisgood/r1sIngHub/main/Scripts/RemoteSpy.lua", true))()
+end})
