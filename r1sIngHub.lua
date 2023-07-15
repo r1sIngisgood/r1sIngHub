21cd64ccfcacf124bbd894e96a162921349e4e4d
r1sIngisgood <91692710+r1sIngisgood@users.noreply.github.com>
2023-07-15 20:34:58 +0300

    actual hub added

---
+++ r1sIngHub.lua
@@ -0,0 +1,18 @@
+local UiLib = require(game:HttpsGet("https://github.com/r1sIngisgood/r1sIngHub/UiLibs/Bracket.lua"))
+
+local Window = UiLib:Window() do
+    Window:ChangeName("r1sIngHub")
+    Window:ChangeSize(UDim2.new(0,500,0,500))
+    Window:ChangePosition(UDim2.new(0.5,0,0.5,0))
+    local ScriptsTab = Window:Tab() do
+        ScriptsTab:ChangeName("Scripts")
+        ScriptsTab:Divider({Text = "Scripts", Side = "Left"})
+
+        local DexButton = ScriptsTab:Button({Name = "Dex Explorer", Side = "Left", Callback = function()
+            require(game:HttpsGet("https://github.com/r1sIngisgood/r1sIngHub/Scripts/DexExplorer.lua"))
+        end})
+        local RemoteSpy = ScriptsTab:Button({Name = "RemoteSpy", Side = "Left", Callback = function()
+            require(game:HttpsGet("https://github.com/r1sIngisgood/r1sIngHub/Scripts/RemoteSpy.lua"))
+        end})
+    end
+end
