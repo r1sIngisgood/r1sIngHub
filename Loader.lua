d0161bbbc5fd8e67de7e463e8861e111440696f4
r1sIngisgood <91692710+r1sIngisgood@users.noreply.github.com>
2023-07-17 20:16:44 +0300

    antiafk deleted and loader updated

--- Loader.lua
+++ Loader.lua
@@ -1,11 +1,13 @@
 local placeId = game.PlaceId
 local gameTable = {}
 
-local scriptString = "https://raw.githubusercontent.com/r1sIngisgood/r1sIngHub/main/Scripts/"
-if not gameTable[placeId] then
-    scriptString = scriptString.."General.lua"
+local githubURL = "https://raw.githubusercontent.com/r1sIngisgood/r1sIngHub/main/Scripts/"
+local scriptString = ""
+local gameFile = gameTable[placeId]
+if type(gameFile) == "string" then
+    scriptString = githubURL..gameTable[placeId]
 else
-    scriptString = scriptString..gameTable[placeId]
+    scriptString = githubURL.."General.lua"
 end
 
 loadstring(game:HttpGet(scriptString))()
--- Scripts/AntiAfk.lua
+++
@@ -1,9 +0,0 @@
-local LocalPlayer = game.Players.LocalPlayer
-local VirtualUser = game:GetService("VirtualUser")
-
-LocalPlayer.Idled:Connect(function()
-    VirtualUser:Button2Down(Vector2.new(), workspace.CurrentCamera.CFrame)
-    task.wait(1)
-    VirtualUser:Button2Up(Vector2.new(), workspace.CurrentCamera.CFrame)
-end)
-
