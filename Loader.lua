1422c92e5c7a2af226f7f29215edb7608114aa7f
r1sIngisgood <91692710+r1sIngisgood@users.noreply.github.com>
2023-07-23 02:32:40 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,6 +2,7 @@ local placeId = game.PlaceId
 local gameTable = {}
 
 local gameFile = gameTable[placeId]
+if identifyexecutor() ~= "Valyse v1" then warn("It's recommended to use Valyse for this script. Some functions may crash/not work") else print("VALYSE CARRIES") end
 if type(gameFile) == "string" then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/r1sIngisgood/r1sIngHub/main/Scripts/"..gameFile))
 else
