c5d158b71e84cd5ac36d3bb0009df54abb85918c
r1sIngisgood <91692710+r1sIngisgood@users.noreply.github.com>
2023-08-02 20:02:40 +0300

    Update AnimeAdventures.lua

--- Scripts/AnimeAdventures.lua
+++ Scripts/AnimeAdventures.lua
@@ -605,8 +605,7 @@ local function Play_Macro()
         local stepTable = chosen_macro_contents[1]
         for i = 1, totalSteps do
             if not macro_playing then break end
-            warn(tostring(getgenv().Options.macro_play_stepdelay_slider.Value + 0.2))
-            task.wait(getgenv().Options.macro_play_stepdelay_slider.Value + 0.2)
+            task.wait(getgenv().Options.macro_play_stepdelay_slider.Value + 0.3)
             local plr_stats = Players.LocalPlayer._stats
             local plr_resource_val = plr_stats.resource
             local cur_task = stepTable[""..i]["type"] or "?"
